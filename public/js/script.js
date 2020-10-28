0e9268849d01f5cbf788fa2dae8236c9777a6bea
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2020-10-28 16:03:06 +1100

    successfull redirection

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+function disable() {
+    document.getElementById("contener_form_infoprojet-Id").style.display = "block";
+}
+function undisable() {
+    document.getElementById("contener_form_infoprojet-Id").style.display = "none";
+    document.getElementById("myForm").reset();
+
+}
+
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mustache = require('mustache-express');
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -15,9 +21,9 @@ const connection = mysql.createConnection({
   password: ""
 });
 
-app.post("/register", function(req, res) {
+app.post("/register", function (req, res) {
   const ie_projet_name = req.body.ie_projet_name;
-  const ie_name = req.body.	ie_name;
+  const ie_name = req.body.ie_name;
   const ie_first_name = req.body.ie_first_name;
   const ie_tribe_name = req.body.ie_tribe_name;
   const ie_phone_number = req.body.ie_phone_number;
@@ -25,7 +31,7 @@ app.post("/register", function(req, res) {
   const ie_object_request = req.body.ie_object_request;
   const ie_ridet = req.body.ie_ridet;
   const ie_isle = req.body.ie_isle;
-        //Table infortion requete//
+  //Table infortion requete//
   //Recapitulatif constructor requete//
   const ie_suppliers_constructor = req.body.ie_suppliers_constructor;
   const ie_post_constructor = req.body.ie_post_constructor;
@@ -51,7 +57,6 @@ app.post("/register", function(req, res) {
   const ie_post_furniture = req.body.ie_post_furniture;
   const ie_estimate_furniture = req.body.ie_estimate_furniture;
 
-
   const user = {
     ie_projet_name: ie_projet_name,
     ie_name: ie_name,
@@ -62,7 +67,7 @@ app.post("/register", function(req, res) {
     ie_object_request: ie_object_request,
     ie_ridet: ie_ridet,
     ie_isle: ie_isle,
-          //Table information//
+    //Table information//
     //Recapitulatif constructor//
     ie_suppliers_constructor: ie_suppliers_constructor,
     ie_post_constructor: ie_post_constructor,
@@ -87,17 +92,16 @@ app.post("/register", function(req, res) {
     ie_suppliers_furniture: ie_suppliers_furniture,
     ie_post_furniture: ie_post_furniture,
     ie_estimate_furniture: ie_estimate_furniture,
-    
   };
 
   connection.query("INSERT INTO users SET ?", user);
-  
-  return res.redirect("success.html");
+
+  return res.render('success', { user : user });
 });
 
 app
-  .get("/", function(req, res) {
-    return res.redirect("index.html");
+  .get("/", function (req, res) {
+    return res.render("home");
   })
   .listen(3000);
 
